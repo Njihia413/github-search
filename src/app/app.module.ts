e2be452f84fc54fadcad53d9b19d665fded0bede
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-29 21:49:36 +0300

    Add footer component and style it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MainpageComponent
+    MainpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
