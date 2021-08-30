4900e203c2b83a549f1ada5b223224c25727f831
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-30 23:25:46 +0300

    Add routing paths for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { MainpageComponent } from './mainpage/mainpage.component';
 import { UserSearchComponent } from './user-search/user-search.component';
 
 const routes: Routes = [
-  { path: '', component: MainpageComponent },
-  { path: 'user-search', component: UserSearchComponent}
+  { path: 'main', component: MainpageComponent },
+  { path: 'user-search', component: UserSearchComponent},
+  { path: '', redirectTo:"/main", pathMatch:"full"}
 
 ];
 
